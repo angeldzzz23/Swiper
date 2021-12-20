732ed7f02b2111610574d32069a1cfad5e5d3c98
angel zambrano <angeldzambrano99@gmail.com>
2021-12-20 02:55:28 -0800

    Added top navigation

--- TinderClone/TinderClone/ViewController.swift
+++ TinderClone/TinderClone/ViewController.swift
@@ -9,44 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // instance properties
+    let topStackView = TopNavigationStackView()
+    let blueView = UIView()
+    let buttonStackView = HomeBottomControlsStackView()
+
+    
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-
-        // this map function, iterates through all of the colors and returns  and array
-        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { color -> UIView in
-            let v = UIView()
-            v.backgroundColor = color
-            return v
-        }
         
         
-        let topStackView = UIStackView(arrangedSubviews: subviews)
-
-        topStackView.distribution = .fillEqually
-        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        let blueView = UIView()
         blueView.backgroundColor = .blue
+        setupLayout()
+        
+        
+    }
     
-        
-        let buttonStackView = HomeBottomControlsStackView()
-        
-        
-        
+    // MARK: setting up the layout
+    fileprivate func setupLayout() {
         let overallStackView  = UIStackView(arrangedSubviews: [topStackView, blueView,buttonStackView])
         overallStackView.axis = .vertical // makes it spand in the vertical axis
-        
-        
-        
         view.addSubview(overallStackView)
-
-        
         overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
-        
-
-        
-        
     }
 
 
--- TinderClone/TinderClone/Views/TopNavigationStackView.swift
+++ TinderClone/TinderClone/Views/TopNavigationStackView.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class TopNavigationStackView: UIStackView {
     
+    let settingsButton = UIButton(type: .system)
+    let messagesButton = UIButton(type: .system)
+    let fireImageView = UIImageView(image: UIImage(named: "app_icon"))
+    
+    
+    // TODO:
+    // Shrink fireImageview without having to use hack
     override init(frame:CGRect) {
         super.init(frame: frame)
         
+        heightAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        fireImageView.contentMode = .scaleAspectFit
+        
+        settingsButton.setImage(UIImage(named: "top_left_profile")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        messagesButton.setImage(UIImage(named: "top_right_messages")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        // try to fix this
+        [settingsButton, UIView(), fireImageView, UIView(), messagesButton].forEach { v in
+            addArrangedSubview(v)
+        }
+        
+        // Read the documentation for this
+        distribution = .equalCentering
+        
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins = .init(top: 0, left: 16, bottom: 0, right: 16)
         
         
+        
+  
         
         
     }
