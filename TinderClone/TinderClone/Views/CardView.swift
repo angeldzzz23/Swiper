b90563d61315681b9078eb3a7a4f5ed9f3f3c036
angel zambrano <angeldzambrano99@gmail.com>
2021-12-20 21:28:47 -0800

    added pan recognizer

--- TinderClone/TinderClone/Views/CardView.swift
+++ TinderClone/TinderClone/Views/CardView.swift
@@ -21,7 +21,44 @@ class CardView: UIView {
         addSubview(imageView)
         imageView.fillSuperview()  // adds contraint to the superview
         
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+        
     }
+    
+ 
+   
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        
+        
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded()
+
+        default:
+            ()
+        }
+        
+    }
+    
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    
+    fileprivate func handleEnded() {
+            
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut) {
+            self.transform = .identity // brings it back to the origin
+        } completion: { _ in
+            
+        }
+    }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
