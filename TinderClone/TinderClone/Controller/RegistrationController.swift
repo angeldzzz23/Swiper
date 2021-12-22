9d089bf1832abd1fdf55ca97534434abf0014fd5
angel zambrano <angeldzambrano99@gmail.com>
2021-12-22 03:18:01 -0800

    dismissing keyboard functionality

--- TinderClone/TinderClone/Controller/RegistrationController.swift
+++ TinderClone/TinderClone/Controller/RegistrationController.swift
@@ -58,26 +58,73 @@ class RegistrationController: UIViewController {
     }()
     
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupGradientLayer()
         
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        setupLayout()
+        setUpNotificationObservers()
+        setupTapGesture()
         
-        let stackView = UIStackView(arrangedSubviews: [
-            selectPhotoButton, fullNameTextField, emailTextField, passwordTextField, RegisterButton
-        ])
-        
-        stackView.axis = . vertical
-        stackView.spacing = 8
-        view.addSubview(stackView)
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
-        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
         
     }
     
+    @objc func handleTapDismiss() {
+        self.view.endEditing(true) // dismiss keyboard
+        // adding animate method
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.view.transform = .identity
+        }
+    
+    }
+    
+    
+    
+    fileprivate func setUpNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self) // can cause a retain cycle
+    }
+    
+    
+    // handles the handling of when a keyboard hides
+    @objc fileprivate func  handleKeyboardHide() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.view.transform = .identity
+        }
+    }
+    
+    //
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        print("kehyboard will show.")
+        // getting the height of the keyboard
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardFrame = value.cgRectValue
+        
+        // how tall the gap is from the register button to the bottom of the screen
+        // the height of the view
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        
+        let difference = keyboardFrame.height - bottomSpace
+        self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8) // adds some padding to this
+
+        
+        
+    }
+    
+    // MARK: Helper functions
     // adding gradient
     fileprivate func  setupGradientLayer() {
         let gradientLayer = CAGradientLayer()
@@ -90,4 +137,24 @@ class RegistrationController: UIViewController {
         gradientLayer.frame = view.bounds
     }
     
+    // variable is created after the selectPhotoButton and other variables are compiled
+    lazy var stackView = UIStackView(arrangedSubviews: [
+        selectPhotoButton,
+        fullNameTextField,
+        emailTextField,
+        passwordTextField,
+        RegisterButton
+        ])
+    
+    fileprivate func setupLayout() {
+        // Do any additional setup after loading the view.
+        
+      
+        
+        stackView.axis = . vertical
+        stackView.spacing = 8
+        view.addSubview(stackView)
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
